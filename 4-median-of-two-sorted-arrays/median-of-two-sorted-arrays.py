552af1c4b49b5008219446485bdb7e15c8b1a524
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2025-05-31 22:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (10.78%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,28 +1,12 @@
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        # Ensure nums1 is the smaller array
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
-
-        x, y = len(nums1), len(nums2)
-        low, high = 0, x
-
-        while low <= high:
-            partitionX = (low + high) // 2
-            partitionY = (x + y + 1) // 2 - partitionX
-
-            maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
-            minRightX = float('inf') if partitionX == x else nums1[partitionX]
-
-            maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
-            minRightY = float('inf') if partitionY == y else nums2[partitionY]
-
-            if maxLeftX <= minRightY and maxLeftY <= minRightX:
-                if (x + y) % 2 == 0:
-                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
-                else:
-                    return max(maxLeftX, maxLeftY)
-            elif maxLeftX > minRightY:
-                high = partitionX - 1
-            else:
-                low = partitionX + 1
+        num3 = nums1 + nums2
+        num3.sort()
+        n = len(num3)
+        if n % 2 == 1:
+            median = num3[n // 2]
+        else:
+            mid1 = num3[n // 2 - 1]
+            mid2 = num3[n // 2]
+            median = (mid1 + mid2) / 2.0
+        return median
