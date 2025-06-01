5780faa8ba5f37f3db9adfa5eb5dd65218a01baa
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2025-06-01 08:43:18 +0530

    .

--- 9 - Palindrome number/code.py
+++
@@ -1,8 +0,0 @@
-# LeetCode Problem: https://leetcode.com/problems/palindrome-number/
-# Difficulty: Easy
-# Tags: Math
-# Approach: inline check if the number is the same as its reverse by converting it to string and by using slicing.
-
-class Solution(object):
-    def isPalindrome(self, x):
-            return str(x) == str(x)[::-1]
